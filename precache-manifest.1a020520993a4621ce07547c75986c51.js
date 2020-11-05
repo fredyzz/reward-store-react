42e6e52b4580538ac027eee3b3297676f6dd8470
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2020-11-05 16:03:46 -0300

    Updates

---
+++ precache-manifest.1a020520993a4621ce07547c75986c51.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e8129694d3d84ac51a3c9e0d0fed1c",
+    "url": "/reward-store-react/index.html"
+  },
+  {
+    "revision": "513063619ef1d19fa383",
+    "url": "/reward-store-react/static/css/main.63c6be6f.chunk.css"
+  },
+  {
+    "revision": "7eb6e4592ce10e32c7be",
+    "url": "/reward-store-react/static/js/2.6e3b63a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reward-store-react/static/js/2.6e3b63a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513063619ef1d19fa383",
+    "url": "/reward-store-react/static/js/main.935e00d5.chunk.js"
+  },
+  {
+    "revision": "2c39b3c0e396e03972da",
+    "url": "/reward-store-react/static/js/runtime-main.2fc1149e.js"
+  },
+  {
+    "revision": "655b05808f71b35ce7ba914e23bb0d53",
+    "url": "/reward-store-react/static/media/cart2.655b0580.svg"
+  },
+  {
+    "revision": "6175aed506faf91af852644f73a4e91a",
+    "url": "/reward-store-react/static/media/coin.6175aed5.svg"
+  },
+  {
+    "revision": "f0b98264ce8c93768fbf37c263885aaa",
+    "url": "/reward-store-react/static/media/header-x1.f0b98264.png"
+  },
+  {
+    "revision": "05714a036e270cfc86b771f4dec585ec",
+    "url": "/reward-store-react/static/media/left-arrow.05714a03.svg"
+  },
+  {
+    "revision": "8caf70015c27d627c7ee2c7fbccb8740",
+    "url": "/reward-store-react/static/media/logo.8caf7001.svg"
+  },
+  {
+    "revision": "8b99ffd0998deb0bbad977c9d9b7a52f",
+    "url": "/reward-store-react/static/media/right-arrow.8b99ffd0.svg"
+  },
+  {
+    "revision": "ad56fb628a38435f7ccf8776c47b796d",
+    "url": "/reward-store-react/static/media/success.ad56fb62.gif"
+  }
+]);
